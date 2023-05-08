4ff4e8d4b356adfa6d000bd66a43c025d0e3626d
Sam Edwards <sam@samedwards.ca>
2023-05-08 14:39:03 -0700

    Don't wrap highlights in main role

--- inc/views/shortcode.php
+++ inc/views/shortcode.php
@@ -1,18 +1,29 @@
+<?php
+
+/**
+ * Output for the highlight shortcode
+ */
+
+?>
 <?php if (!empty($highlights_ordered)) : ?>
-    <div class="row highlights" role="main" id="primary">
+    <div class="row highlights">
         <?php foreach ($highlights_ordered as $column_number => $post) : ?>
             <div class="third lede-<?= $column_number ?>">
                 <?php
-                setup_postdata($GLOBALS['post'] = &$post);
-                $permalink = null;
+                setup_postdata($GLOBALS['post'] = $post);
                 $linked_id = get_post_meta($post->ID, '_coop_highlights_linked_post', true);
-                if ($linked_id) {
-                    $permalink = get_permalink($linked_id);
-                }
+                $permalink = $linked_id ? get_permalink($linked_id) : false;
                 ?>
                 <?php echo $permalink ? '<a href="' . $permalink . '">' : '' ?>
-                <?php the_title('<h3>', '</h3>'); ?>
-                <?php the_content(); ?>
+                    <?php the_title('<h3>', '</h3>'); ?>
+                    <?php
+                    /**
+                     * Maintaining functionality of wrapping the content in the
+                     * anchor tag, even though that's less than useful in most
+                     * cases
+                     */
+                    ?>
+                    <?php the_content(); ?>
                 <?php echo $permalink ? '</a>' : '' ?>
             </div>
         <?php endforeach;
